c765567c425af28f53ebb2528c0a398290bf8bc0
="Damanveer Singh <=>
2017-06-20 11:27:29 -0400

    Added Route and Link for Github

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import{ Switch, Route, NavLink } from 'react-router-dom';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h3>Ain't no party like an</h3>
-          <h1>API Party</h1>
+          <div className="App-headings">
+            <h3>Ain't no party like an</h3>
+            <h1>API Party</h1>
+          </div>
+          <ul className="nav-links">
+            <li><NavLink to="/github">Github API</NavLink></li>
+
+          </ul>
         </div>
+        
+        
+        <Switch>
+          <Route path="/github" render={()=><p>GITHUB!</p>} />
+          <Route render={()=> <p>To begin, select one of the APIs above</p>} />
+          
+        </Switch>
       </div>
     );
   }
